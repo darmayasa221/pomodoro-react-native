f9aa01498e64a22e9b318a7014399c1de2dac4bb
darmayasa221 <darmayasa221@gmail.com>
2023-04-14 16:20:37 +0800

    FE-UI-02 header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,52 @@
+import React, {memo} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import ButtonCostum from '../UI/ButtonCostum';
+import FontAwsome5Icon from 'react-native-vector-icons/FontAwesome5';
+import FontistoIcon from 'react-native-vector-icons/Fontisto';
+
+const Header = () => {
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.headerText}>Pomofocus</Text>
+      <View style={styles.rightSide}>
+        <ButtonCostum style={styles.button}>
+          <FontAwsome5Icon name="user-circle" size={20} color={'white'} />
+        </ButtonCostum>
+        <ButtonCostum style={styles.button}>
+          <FontistoIcon name="player-settings" size={20} color={'white'} />
+        </ButtonCostum>
+        <ButtonCostum style={styles.button}>
+          <FontAwsome5Icon name="chart-bar" size={20} color={'white'} />
+        </ButtonCostum>
+      </View>
+    </View>
+  );
+};
+
+export default memo(Header);
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    marginTop: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    borderBottomColor: '#424242',
+    borderBottomWidth: 0.25,
+    paddingBottom: 10,
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: 'white',
+  },
+  rightSide: {
+    flexDirection: 'row-reverse',
+    columnGap: 20,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+  },
+});
