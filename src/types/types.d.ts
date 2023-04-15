501fb3f5ff930e7c8e9d33f6920b332f1240b222
darmayasa221 <darmayasa221@gmail.com>
2023-04-15 22:50:43 +0800

    FE-UI-02 merge

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,9 +1,13 @@
-//button
-
 import {PropsWithChildren} from 'react';
-
+// button
 type ButtonCostumProps = PropsWithChildren<{
   style: {};
 }>;
+type ButtonPrimaryProps = PropsWithChildren<{
+  text: string;
+  onPress?: () => void;
+  styleView?: {};
+  styleText?: {};
+}>;
 
-export {ButtonCostumProps};
+export {ButtonCostumProps, ButtonPrimaryProps};
