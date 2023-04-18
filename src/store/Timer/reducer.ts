6e2daf43bb62173af3031220b20270dc5c79ed9d
darmayasa221 <darmayasa221@gmail.com>
2023-04-18 16:27:50 +0800

    adding TIMER_START features

--- src/store/Timer/reducer.ts
+++ src/store/Timer/reducer.ts
@@ -28,32 +28,18 @@ const timerReducer = (
     newState.data[2].activeMenu = true;
     return newState;
   }
-  // if (action.type === 'POMODORO_START') {
-  //   const newState = {...state};
-  //   newState.data[0].activeMenu = false;
-  //   newState.data[1].activeMenu = true;
-  //   newState.data[2].activeMenu = false;
-  //   return newState;
-  // }
-  // if (action.type === 'SHORT_BREAK_START') {
-  //   const newState = {...state};
-  //   newState.data[0].activeMenu = true;
-  //   newState.data[1].activeMenu = false;
-  //   newState.data[2].activeMenu = false;
-  //   return newState;
-  // }
-  // if (action.type === 'LONG_BREAK_START') {
-  //   const newState = {...state};
-  //   newState.data[0].activeMenu = true;
-  //   newState.data[1].activeMenu = false;
-  //   newState.data[2].activeMenu = false;
-  //   return newState;
-  // }
   if (action.type === 'START_TIMER') {
     const newState = {...state};
-    const payload = action.payload;
-
-    return state;
+    const item = newState.data.find(({name}) => name === action.payload?.name);
+    const itemIndex = newState.data.findIndex(
+      ({name}) => name === action.payload?.name,
+    );
+    const newItem = {
+      ...item,
+      time: action.payload?.time,
+    };
+    newState.data[itemIndex] = newItem;
+    return newState;
   }
   return state;
 };
