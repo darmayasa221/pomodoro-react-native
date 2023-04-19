470d0e72c990e5ffa3eb7643c4d33b7de13d6d17
darmayasa221 <darmayasa221@gmail.com>
2023-04-19 22:30:35 +0800

    adding task property

--- src/types/store/task/task.d.ts
+++ src/types/store/task/task.d.ts
@@ -3,13 +3,19 @@ export type TaskItemType = {
   name?: string;
   activedTask?: boolean;
 };
+export type TaskSelectedType = TaskItemType & {
+  isSelected?: boolean;
+};
 export type TaskInitialStateType = {
   data: Array<TaskItemType>;
+  selected: TaskSelectedType;
 };
-export type ActionType = 'ADD_TASK' | 'ACTIVED_TASK';
+export type ActionType = 'ADD_TASK' | 'ACTIVED_TASK' | 'SELECT_TASK';
 export type TaskActionType = {
   type: ActionType;
-  payload: TaskItemType;
+  payload?: TaskItemType & {
+    index?: number;
+  };
 };
 export type TaskContextType = {
   state: TaskInitialStateType;
