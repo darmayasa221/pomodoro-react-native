9ab10cdc106c4f6df984e093e4110b2f35a5cd79
darmayasa221 <darmayasa221@gmail.com>
2023-04-19 22:31:01 +0800

    adding new logic for ACTIVED_TASK at recuder

--- src/store/Task/reducer.ts
+++ src/store/Task/reducer.ts
@@ -2,6 +2,7 @@ import {
   TaskActionType,
   TaskInitialStateType,
   TaskItemType,
+  TaskSelectedType,
 } from '../../types/store/task/task';
 
 const taskReducer = (
@@ -11,14 +12,30 @@ const taskReducer = (
   if (action.type === 'ADD_TASK') {
     const newState = {...state};
     const task: TaskItemType = {
-      activedTask: false,
-      name: action.payload.name,
+      activedTask: true,
+      name: action.payload?.name as string,
     };
     newState.data.push(task);
     return newState;
   }
   if (action.type === 'ACTIVED_TASK') {
-    return state;
+    const newState = {...state};
+    const actived = newState.data[action.payload?.index as number].activedTask;
+    newState.data[action.payload?.index as number].activedTask = actived
+      ? false
+      : true;
+    return newState;
+  }
+  if (action.type === 'SELECT_TASK') {
+    const newState = {...state};
+    const taskSelected: TaskSelectedType = {
+      ...newState.selected,
+      name: action.payload?.name,
+      activedTask: action.payload?.activedTask,
+      isSelected: true,
+    };
+    newState.selected = taskSelected;
+    return newState;
   }
   return state;
 };
