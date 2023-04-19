782d1b31856313b539bebf03744fe674d334ad1e
darmayasa221 <darmayasa221@gmail.com>
2023-04-19 13:38:48 +0800

    change type onCount

--- src/types/modal.d.ts
+++ src/types/modal.d.ts
@@ -1,12 +1,15 @@
-import {TimerItemType} from './store/timer/timer';
-
+import {Dispatch} from 'react';
+import {TimerActionType, TimerItemType} from './store/timer/timer';
 // modal type component
 export type SettingTimerProps = {
   color: string;
   data: Array<TimerItemType>;
-  onCount: (action: TimerActionType) => void;
+  onCount: Dispatch<TimerActionType>;
   onSave: () => void;
 };
 
-export type SettingTimerItemProps = Pick<TimerItemType, 'name' | 'time'> &
+export type SettingTimerItemProps = Pick<
+  TimerItemType,
+  'name' | 'defaultTime'
+> &
   Pick<SettingTimerProps, 'onCount'>;
