f9e4e1ba3a67b6a09f69cfa6aef99c02610a566e
darmayasa221 <darmayasa221@gmail.com>
2023-04-19 13:42:18 +0800

    Creating function tunOn and TurnOff modal

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,5 +1,5 @@
-import React, {useContext, memo, useCallback, useMemo} from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useContext, memo, useCallback, useMemo, useState} from 'react';
+import {SafeAreaView, StyleSheet, View, Modal} from 'react-native';
 
 import Header from '../components/Header/Header';
 import Timer from '../components/Timer/Timer';
@@ -9,8 +9,10 @@ import TaskFooter from '../components/Tasks/TaskFooter';
 import TimerContext from '../store/Timer/context';
 import {TimerActionType, TimerItemType} from '../types/store/timer/timer';
 import checkActiveMenu from '../utils/checkActiveMenu';
+import SettingTimer from '../components/Modal/SettingTimer';
 
 const MainScreen = () => {
+  const [isModalActived, setIsModalActived] = useState<boolean>(false);
   const {state: timerState, dispatch: timerDispatch} = useContext(TimerContext);
   const isActivedMemo = useMemo(
     () => checkActiveMenu(timerState),
@@ -23,6 +25,12 @@ const MainScreen = () => {
     },
     [timerDispatch],
   );
+  const turnOnModalHandler = useCallback(() => {
+    setIsModalActived(() => true);
+  }, []);
+  const turnOffModalHandler = useCallback(() => {
+    setIsModalActived(() => false);
+  }, []);
   return (
     <View
       style={{
@@ -30,7 +38,15 @@ const MainScreen = () => {
         backgroundColor: isActivedMemo?.color,
       }}>
       <SafeAreaView />
-      <Header />
+      <Modal visible={isModalActived} animationType="slide">
+        <SettingTimer
+          onCount={timerDispatch}
+          onSave={turnOffModalHandler}
+          color={isActivedMemo?.color as string}
+          data={timerMemo}
+        />
+      </Modal>
+      <Header settingOnPress={turnOnModalHandler} />
       <Timer
         menu={timerMemo}
         onPress={menuHandler}
