5f3c68510d1d028960676d9cae8d4c343882de54
darmayasa221 <darmayasa221@gmail.com>
2023-04-19 20:58:59 +0800

    implement reducer wide state at context

--- src/store/Task/index.tsx
+++ src/store/Task/index.tsx
@@ -1,9 +1,20 @@
-import {FC} from 'react';
+import React, {FC, useMemo, useReducer} from 'react';
 import {TaskContextProviderType} from '../../types/store/task/task';
 import TaskContext from './context';
+import taskReducer from './reducer';
 
 const TaskContextProvider: FC<TaskContextProviderType> = ({children}) => {
-  return <TaskContext.Provider value={{}}>{children}</TaskContext.Provider>;
+  const [state, dispatch] = useReducer(taskReducer, {data: []});
+  const stateMemo = useMemo(
+    () => ({
+      state,
+      dispatch,
+    }),
+    [state, dispatch],
+  );
+  return (
+    <TaskContext.Provider value={stateMemo}>{children}</TaskContext.Provider>
+  );
 };
 
 export default TaskContextProvider;
